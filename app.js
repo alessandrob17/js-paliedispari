5982dfb98ac910ac926e942a0b6f3f0e88f7b489
alessandrob17 <a.baldipardi@gmail.com>
2023-02-08 16:59:43 +0100

    FEAT function

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 // INSERIRE UNA PAROLA 
 
 let parola = prompt('Inserisci una parola');
+
+
+// FUNZIONE
+
+function pPalindroma(parola) {
+    let parolaInversa = parola.split("").reverse().join("");
+  return parola === parolaInversa;
+}
+
